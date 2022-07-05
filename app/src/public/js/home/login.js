5f68a94795222ce958c766eb1a6305f59c684625
naingyu <sktmdgus1212@naver.com>
2022-07-05 20:28:50 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,7 @@
 "use strict"
+
+const { post } = require("../../../routes/home");
+
 //dom을 통해 html의 데이터를 js로 제어
 
 const id = document.querySelector("#id"); //html id="id" 불러옴
@@ -12,5 +15,13 @@ function login(){
         id: id.value,
         psword: psword.value,
     }
-    console.log(req);
+    //console.log(req);
+    //console.log(JSON.stringify(req));
+    fetch("/login", {
+        method: post,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+    });
 }
