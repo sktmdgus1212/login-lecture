f4ad1ff4b35d31f95aaa09c8ea9bbf5e0e9970fb
naingyu <sktmdgus1212@naver.com>
2022-07-05 20:16:24 +0900

    dom을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 "use strict"
+//dom을 통해 html의 데이터를 js로 제어
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"); //html id="id" 불러옴
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    }
+    console.log(req);
+}
