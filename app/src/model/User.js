d241485db9408162a3410981aa8a36208b067f98
naingyu <sktmdgus1212@naver.com>
2022-07-11 19:29:14 +0900

    백엔드 맛보기 최종 최적화

--- app/src/model/User.js
+++ app/src/model/User.js
@@ -10,16 +10,14 @@ class User{
     async login(){
         const client = this.body;
         try{
-            const {id, psword} = await UserStoarage.getUserInfo(client.id);
-            if(id){
-                if(id === client.id && psword === client.psword){
+            const user = await UserStoarage.getUserInfo(client.id);
+            if(user){
+                if(user.id === client.id && user.psword === client.psword){
                     return {success: true};   
                 }
                 return {success: false, msg: "비밀번호가 틀렸습니다."};
             }
-            else{
-                return {success: false, msg: "존재하지 않는 계정입니다."};
-            }
+            return {success: false, msg: "존재하지 않는 계정입니다."};
         }
         catch(err){
             return {success: false, err};
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -8,15 +8,15 @@ const logger = require("../../config/logger");
 const output = {
     hello: (req, res) =>{
         //res.send();
-        logger.info("GET / 200 -홈 화면으로 이동");
+        logger.info("GET / 304 -홈 화면으로 이동");
         res.render("home/index");
     },
     login: (req, res) =>{
-        logger.info("GET /login 200 -로그인 화면으로 이동");
+        logger.info("GET /login 304 -로그인 화면으로 이동");
         res.render("home/login");
     },
     register: (req, res) =>{
-        logger.info("GET /register 200 -회원가입 화면으로 이동");
+        logger.info("GET /register 304 -회원가입 화면으로 이동");
         res.render("home/register");
     }
 }
@@ -26,24 +26,41 @@ const process = {
     login: async (req, res) => {
         const user = new User(req.body);
         const response = await user.login();
-        if(response.err){
-            logger.error(`POST /login 200 ResponseL "success: ${response.success}, msg: ${response.err}"`);
-        }
-        else{
-            logger.info(`POST /login 200 ResponseL "success: ${response.success}, msg: ${response.msg}"`);
-        }
-        return res.json(response);
+
+        const url = {
+            method: 'POST',
+            path: 'login',
+            status: response.err? 400 : 200,
+            //200~: 정상 응답
+            //300~: 페이지 이동
+            //400~: 클라이언트 실수
+            //500~: 서버 실수
+        };
+
+        log(response, url);
+        return res.status(url.status).json(response);
     },
     register: async (req, res) => {
         const user = new User(req.body);
         const response = await user.register();
-        if(response.err){
-            logger.error(`POST /register 200 ResponseL "success: ${response.success}, msg: ${response.err}"`);
-        }
-        else{
-            logger.info(`POST /register 200 ResponseL "success: ${response.success}, msg: ${response.msg}"`);
-        }
-        return res.json(response);
+       
+        const url = {
+            method: 'POST',
+            path: 'register',
+            status: response.err? 400 : 200,
+        };
+
+        log(response, url);
+        return res.status(url.status).json(response);
+    }
+}
+
+const log = (response, url) => {
+    if(response.err){
+        logger.error(`${url.method} /${url.path} ${url.status} Response: ${response.success} ${response.err}"`);
+    }
+    else{
+        logger.info(`${url.method} /${url.path} ${url.status} Response: ${response.success} ${response.msg||''}"`);
     }
 }
 
